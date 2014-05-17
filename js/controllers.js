29a4a0dced9c7744f692109caf57539d19122860
Alex Kalderimis <alexkalderimis@gmail.com>
2014-05-17 16:25:21 +0100

    Separated input from results.

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,13 @@
 define(['underscore', './controllers/search-results'], function (_, SearchResultsCtrl) {
 	var Controllers = angular.module('multimine-search-tool.controllers', []);
-	Controllers.controller('MainCtrl', SearchResultsCtrl);
+
+	Controllers.controller('DemoCtrl', ['$scope', function (scope) {
+    scope.step = {data: {}};
+  }]);
+
+	Controllers.controller('SearchInputCtrl', ['$scope', function (scope) {
+  }]);
+
+	Controllers.controller('SearchResultsCtrl', SearchResultsCtrl);
+
 });
