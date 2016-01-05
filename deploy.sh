8d18b0d83603f8a9036b2cace7674ece9d222557
Yo Yehudi <yochannah@gmail.com>
2016-01-05 16:38:16 +0000

    added travis deploy files

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### This script is used by travis to automatically deploy new
+### master branch pushes to gh-pages
+
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+git init
+git config user.name "Travis CI"
+git config user.email "travis@fakemail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+
+git add -f bower_components
+
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
